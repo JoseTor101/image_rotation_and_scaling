d56b9389b836110cf039e56a54e24d9bcea376a3
josetor101 <josetordecilla800@gmail.com>
2025-03-25 18:41:17 -0500

    Fixed sintax

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
-#include <vector>
-#include <iostream>
-#include <cstring>  // For strcmp
-#include <cstdlib>  // For std::stoi()
 #include "image.h"
+#include <cstdlib> // For std::stoi()
+#include <cstring> // For strcmp
+#include <iostream>
+#include <vector>
 
 int main(int argc, char *argv[]) {
-    Image img;
-    img.image("./test/fish.jpg");
+  Image img;
+  img.image("./test/fish.jpg");
 
-    int angle = 0;  // Default angle
+  int angle = 0; // Default angle
 
-    // Parse command line arguments
-    for (int i = 1; i < argc; ++i) {
-        if (strcmp(argv[i], "-angulo") == 0 && i + 1 < argc) {
-            angle = std::stoi(argv[i + 1]);  // Convert string to int
-        }
+  // Parse command line arguments
+  for (int i = 1; i < argc; ++i) {
+    if (strcmp(argv[i], "-angulo") == 0 && i + 1 < argc) {
+      angle = std::stoi(argv[i + 1]); // Convert string to int
     }
+  }
 
-    // Rotate the image with the given angle
-    img.rotateImage(angle);
+  // Rotate the image with the given angle
+  img.rotateImage(angle);
 
-    return 0;
+  return 0;
 }
