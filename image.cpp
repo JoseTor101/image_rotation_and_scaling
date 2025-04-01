42cb109637a054b19be25ae48f5ddc3032cc2af6
Jose Tordecilla <70181603+JoseTor101@users.noreply.github.com>
2025-04-01 15:05:15 -0500

    Feature/multi transformations (#12)
    * Fixed sintax
    
    * Combined matrix and output

--- image.cpp
+++ image.cpp
@@ -1,9 +1,19 @@
 #include "image.h"
+#include <chrono>
 #include <Eigen/Dense>
 #include <cmath>
 #include <cstring>
+#include <iostream>
+#include <fstream>
+#include <sys/resource.h>
 
 using namespace std;
+
+double getMemoryUsageMB() {
+  struct rusage usage;
+  getrusage(RUSAGE_SELF, &usage);
+  return usage.ru_maxrss / 1024.0; // Convert KB to MB
+}
 
 // Constructor definition
 // Image::Image() : data(nullptr), width(0), height(0), channels(0) {}
@@ -14,10 +24,16 @@ void Image::image(const char *path) {
   data = stbi_load(path, &width, &height, &channels, 0);
 
   if (data) {
-    cout << "Imagen cargada: " << width << "x" << height << " con " << channels
-         << " canales.\n";
+    cout << "+---------------------------+\n";
+    cout << "       Imagen Cargada      \n";
+    cout << "+---------------------------+\n";
+    cout << " Dimensiones: " << width << " x " << height << "\n";
+    cout << " Canales: " << channels << " (RGB) \n";
   } else {
-    cerr << "Error al cargar la imagen: " << stbi_failure_reason() << "\n";
+    cerr << "+---------------------------+\n";
+    cerr << "   Error al cargar imagen  \n";
+    cerr << "+---------------------------+\n";
+    cerr << " Motivo: " << stbi_failure_reason() << "\n";
   }
 }
 
@@ -89,7 +105,7 @@ void Image::rotateImage(int angle) {
     }
   }
 
-  cout << "Rotation complete!" << endl;
+  cout << "Rotación completa" << endl;
 
   rotatedImage.saveImage("./output/rotated.jpg");
 }
@@ -145,17 +161,109 @@ void Image::scaleImage(float scaleFactor) {
   scaledImage.saveImage("./output/scaled.jpg");
 }
 
+void Image::transformImage(const string &inputPath, const string &outputPath, int angle, float scaleFactor, bool buddySystem) {
+  using namespace std::chrono;
+
+  // Start measuring time
+  auto start = high_resolution_clock::now();
+
+  // Get memory usage before transformation
+  double memoryBefore = getMemoryUsageMB();
+
+  // Load the image
+  image(inputPath.c_str());
+
+  if (scaleFactor <= 0) {
+    cerr << "El factor de escala debe ser mayor que 0." << endl;
+    return;
+  }
+
+  cout << "+---------------------------+\n";
+  cout << "       PROCESAMIENTO        \n";
+  cout << "+---------------------------+\n";
+  cout << " Archivo entrada: " << inputPath << " \n";
+  cout << " Archivo salida: " << outputPath << " \n";
+  cout << " Modo de asignación de memoria : " << (buddySystem ? "Buddy system" : "Sin Buddy system") << " \n";
+  cout << "+---------------------------+\n";
+  cout << " Dimensiones originales: " << width << "x" << height << " \n";
+
+  double radians = angle * M_PI / 180.0;
+  
+  Eigen::Matrix2f transformMatrix;
+  transformMatrix << scaleFactor * cos(radians), -scaleFactor * sin(radians),
+                     scaleFactor * sin(radians),  scaleFactor * cos(radians);
+  
+  int newWidth = abs(width * scaleFactor * cos(radians)) + abs(height * scaleFactor * sin(radians));
+  int newHeight = abs(width * scaleFactor * sin(radians)) + abs(height * scaleFactor * cos(radians));
+  cout << " Dimensiones finales: " << newWidth << "x" << newHeight << " \n";
+  cout << " Canales: " << channels << " (RGB)\n";
+  cout << " Ángulo de rotación: " << angle << " grados\n";
+  cout << " Factor de escalado: " << scaleFactor << " \n";
+
+  Image transformedImage;
+  transformedImage.data = new unsigned char[newWidth * newHeight * channels]();
+  transformedImage.width = newWidth;
+  transformedImage.height = newHeight;
+  transformedImage.channels = channels;
+
+  Eigen::Vector2f centerOriginal(width / 2.0, height / 2.0);
+  Eigen::Vector2f centerNew(newWidth / 2.0, newHeight / 2.0);
+
+  for (int i = 0; i < newHeight; i++) {
+      for (int j = 0; j < newWidth; j++) {
+          Eigen::Vector2f newCoords(j, i);
+          Eigen::Vector2f oldCoords = transformMatrix.inverse() * (newCoords - centerNew) + centerOriginal;
+
+          int x = round(oldCoords[0]);
+          int y = round(oldCoords[1]);
+
+          if (x >= 0 && x < width && y >= 0 && y < height) {
+              for (int c = 0; c < channels; c++) {
+                  transformedImage.data[(i * newWidth + j) * channels + c] =
+                      data[(y * width + x) * channels + c];
+              }
+          } else {
+              for (int c = 0; c < channels; c++) {
+                  transformedImage.data[(i * newWidth + j) * channels + c] = 0;
+              }
+          }
+      }
+  }
+
+  // End measuring time
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<milliseconds>(stop - start);
+
+  // Get memory usage after transformation
+  double memoryAfter = getMemoryUsageMB();
+  double memoryUsed = memoryAfter - memoryBefore;
+
+  cout << "+---------------------------+\n";
+  cout << "   TIEMPO DE PROCESAMIENTO   \n";
+  cout << "+---------------------------+\n";
+  
+  cout << "- Sin Buddy system: " << duration.count() << " ms" << endl;
+  cout << "- Con Buddy system: " << "[ ]" << " ms" << endl;
+  
+  // Display memory usage
+  cout << "- Memoria utilizada: " << memoryUsed << " MB\n";
+
+  transformedImage.saveImage(outputPath);
+}
+
+
+
 // Save the transformed image
 void Image::saveImage(const string &outputPath) {
   if (!data) {
-    cerr << "No image data available to save!\n";
+    cerr << "[ERROR] No hay datos de imagen disponibles para guardar\n";
     return;
   }
 
   if (stbi_write_jpg(outputPath.c_str(), width, height, channels, data, 100)) {
-    cout << "Transformed image saved to " << outputPath << "\n";
+    cout << "[INFO] Imagen guardada correctamente en " << outputPath << "\n";
   } else {
-    cerr << "Failed to save image.\n";
+    cerr << "[ERROR] Error al guardar la imagen \n";
   }
 }
 
--- main.cpp
+++ main.cpp
@@ -1,34 +1,39 @@
 #include "image.h"
-#include <cstdlib> // For std::stoi()
-#include <cstring> // For strcmp
+#include <cstdlib>  // For std::stoi()
+#include <cstring>  // For strcmp
 #include <iostream>
 #include <vector>
+#include <locale>
 
 int main(int argc, char *argv[]) {
-  Image img;
-  img.image("./test/fish.jpg");
 
-  int angle = 0; // Default angle
-  float scaleFactor = 1; //Default scale factor
+    Image img;
+    int angle = 0;
+    float scaleFactor = 1.0f;
+    bool buddySystem = false;
+    std::string inputPath = "./test/fish.jpg";
+    std::string outputPath = "./output/output.jpg";
 
-  // Parse command line arguments
-  for (int i = 1; i < argc; ++i) {
-    if (strcmp(argv[i], "-angulo") == 0 && i + 1 < argc) {
-      angle = std::stoi(argv[i + 1]); // Convert string to int
-    } else if (strcmp(argv[i], "-escala") == 0 && i + 1 < argc) {
-      scaleFactor = std::stof(argv[i + 1]);  // Convert string to float
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-angulo") == 0 && i + 1 < argc) {
+            angle = std::stoi(argv[i + 1]);
+        } 
+        else if (strcmp(argv[i], "-escalar") == 0 && i + 1 < argc) {
+          scaleFactor = std::stof(argv[i + 1]);
+        } 
+        else if (strcmp(argv[i], "-entrada") == 0 && i + 1 < argc) {
+            inputPath = argv[i + 1];
+        } 
+        else if (strcmp(argv[i], "-salida") == 0 && i + 1 < argc) {
+            outputPath = argv[i + 1];
+        }
+        else if (strcmp(argv[i], "-buddy") == 0) {
+            buddySystem = true;
+        }
     }
-  }
 
-  // Rotate the image with the given angle only if angle different than 0.
-  if (angle != 0) {
-    img.rotateImage(angle);
-  }
+    // Apply transformations
+    img.transformImage(inputPath, outputPath, angle, scaleFactor, buddySystem);
 
-  // Scale the image with the given scale factor only if it's different than 1.
-  if (scaleFactor != 1) {
-      img.scaleImage(scaleFactor);
-  }
-
-  return 0;
+    return 0;
 }
